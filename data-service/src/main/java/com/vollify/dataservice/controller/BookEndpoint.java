af5db106b3f96ff66fc7ccf2990c3fbe5f82bfff
Alois Vollmaier <alois.vollmaier.extern@rewe-group.com>
2022-08-27 15:21:15 +0200

    Create a data service for gateway tests

---
+++ data-service/src/main/java/com/vollify/dataservice/controller/BookEndpoint.java
@@ -0,0 +1,24 @@
+package com.vollify.dataservice.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.vollify.dataservice.model.Book;
+
+/**
+ * @author : Alois Vollmaier (A199165)
+ * @since : 27.08.2022, Sat
+ **/
+
+@RequestMapping(value = "/api/v1/books", produces = "application/json")
+public interface BookEndpoint {
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  ResponseEntity<List<Book>> getBooks();
+}
---
+++ data-service/src/main/java/com/vollify/dataservice/service/BookService.java
@@ -0,0 +1,29 @@
+package com.vollify.dataservice.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.vollify.dataservice.model.Book;
+
+/**
+ * @author : Alois Vollmaier (A199165)
+ * @since : 27.08.2022, Sat
+ **/
+@Service
+public class BookService {
+
+  public List<Book> getBooks() {
+    List<Book> books = new ArrayList<>();
+
+    books.add(new Book("Harry Potter", LocalDate.of(1997, 8, 27)));
+    books.add(new Book("Lord of the Rings", LocalDate.of(1954, 8, 27)));
+    books.add(new Book("The Hobbit", LocalDate.of(1937, 8, 27)));
+    books.add(new Book("The Catcher in the Rye", LocalDate.of(1951, 8, 27)));
+
+    return books;
+  }
+
+}
