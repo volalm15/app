a1dd772d73775cfe3c9dacda522c54afe97e11cc
Alois Vollmaier <alois.vollmaier.extern@rewe-group.com>
2022-08-27 10:58:00 +0200

    Add some config

--- discovery-service/src/main/java/com/vollify/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/vollify/discoveryservice/DiscoveryServiceApplication.java
@@ -2,8 +2,11 @@ package com.vollify.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
